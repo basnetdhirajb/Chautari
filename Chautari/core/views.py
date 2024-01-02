5b3d69748971a6c08efb48143c448029bab9e3f4
Dhiraj B Basnet <basnetdhirajb@gmail.com>
2024-01-01 23:07:47 -0600

    download image, user suggestions, post feed

--- Chautari/core/views.py
+++ Chautari/core/views.py
@@ -4,14 +4,64 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Profile, Post,LikePost, FollowersCount
 from django.http import HttpResponse
+from itertools import chain
+import random
 
 # Create your views here.
 @login_required(login_url='signin')
 def index(request):
     user_object =  User.objects.get(username = request.user.username)
     user_profile = Profile.objects.get(user = user_object)
-    posts = Post.objects.all()
-    return render(request,'index.html',{'user_profile':user_profile,'posts':posts})
+    
+    user_following_list = [] #All the users that the logged in user is following
+    feed = [] #Post of the people the current logged in user is following
+    
+    user_following = FollowersCount.objects.filter(follower = request.user.username)
+    
+    for users in user_following:
+        user_following_list.append(users.user)
+    
+    for usernames in user_following_list:
+        userId = User.objects.get(username = usernames)
+        feed_list = Post.objects.filter(user=userId)
+        feed.append(feed_list)
+    
+    feed_list = list(chain(*feed))
+    
+    
+    #User suggestions
+    all_users = User.objects.all()
+    user_following_all = []
+    
+    for user in user_following:
+        user_list = User.objects.get(username = user.user)
+        user_following_all.append(user_list)
+    
+    #The following list will have those users that are not part of user_following_all
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))] #Users the logged in user is not following including himself
+    
+    #removing current user from the list
+    current_user = User.objects.filter(username = request.user.username)
+    #All the values in new suggestions list is to be saved into this list except the instance in which x is current user
+    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+    
+    #Randomly choose the users
+    random.shuffle(final_suggestions_list)
+    username_profile = []
+    username_profile_list = []
+    
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+    
+    for ids in username_profile:
+        profile_list = Profile.objects.filter(id_user = ids)
+        username_profile_list.append(profile_list)
+    
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+        
+    
+    
+    return render(request,'index.html',{'user_profile':user_profile,'posts':feed_list,'suggestions': suggestions_username_profile_list[:4]})
 
 def signin(request):
     
@@ -177,3 +227,28 @@ def follow(request):
             return redirect('/profile/'+user)
     else:
         return redirect('/')
+
+@login_required(login_url='signin')
+def search(request):
+    
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    
+    if request.method=='POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username) #Looking for matching usernames in the database
+        
+        username_profile = []
+        username_profile_list = []
+        
+        for users in username_object:
+            username_profile.append(users.id)
+        
+        for ids in username_profile:
+            profile_list = Profile.objects.filter(id_user = ids)
+            username_profile_list.append(profile_list)
+        
+        username_profile_list = list(chain(*username_profile_list))
+        
+        
+    return render(request, 'search.html',{'username_profile_list':username_profile_list,'user_profile':user_profile})
